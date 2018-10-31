d1e85bb342d28d613f2522f5a84a8de3dd826892
MyungsuWoo <master.sy90@gmail.com>
2018-10-31 23:00:20 +0900

    add route function for returning member's data

--- routes/member.js
+++ routes/member.js
@@ -1,1 +1,23 @@
+var express = require('express');
+var db = require('../db');
+var router = express.Router();
 
+// member/:phone
+router.get(':phone', function(req, res, next) {
+    var phone = req.params.phone;
+    
+    var sql = "SELECT * " + "FROM bestfood_member " + "WHERE PHONE = ? LIMIT 1;";
+    console.log("sql : " + sql);
+    
+    db.get().query(sql, phone, function (err, rows) {
+        console.log("rows : " + JSON.stringify(rows));
+        console.log("row.length : " + rows.length);
+        if (rows.length > 0) {
+            res.json(rows[0]);
+        } else {
+            res.sendStatus(400);
+        }
+    });
+});
+
+module.exports = router;
