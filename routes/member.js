24e2a4cfc1aad502cc400839304ba012b93e7b3d
MyungsuWoo <master.sy90@gmail.com>
2018-11-07 16:50:57 +0900

    save MemberItemInfo

--- routes/member.js
+++ routes/member.js
@@ -20,28 +20,52 @@ router.get(':phone', function(req, res, next) {
     });
 });
 
+// member/info
 router.post('/phone', function(req, res) {
     var phone = req.body.phone;
+    var name = req.body.name;
+    var sextype = req.body.sextype;
+    var birthday = req.body.birthday;
+    
+    console.log({name, sextype, birthday, phone});
     
     var sql_count = "SELECT COUNT(*) AS CNT FROM bestfood_member WHERE PHONE = ?;";
-    console.log("sql_count : " + sql_count);
     
-    var sql_insert = "INSERT INTO bestfood_member (phone) VALUES(?);";
+    var sql_insert = "INSERT INTO bestfood_member (phone, name, sextype, birthday) VALUES(?, ?, ?, ?);";
     
-    db.get()query(sql_count, phone, function (err, rows) {
-        console.log(rows);
-        console.log(rows[0].cnt);
-        
+    var sql_update = "UPDATE bestfood_member SET name = ?, sextype = ?, birthday = ? WHERE phone = ?;";
+    
+    var sql_select = "SELECT seq FROM bestfood_member WHERE phone = ?;";
+    
+    db.get().query(sql_count, phone, function (err, rows) {
         if (rows[0].cnt > 0) {
-            return res.sendStatus(400);
+            console.log("sql_update : " + sql_update);
+            
+            db.get().query(sql_update, [name, sextype, birthday, phone], function (err, result) {
+                if (err) {
+                    return res.sendStatus(400);
+                }
+                console.log(result);
+                
+                db.get().query(sql_select, phone, function (err, rows) {
+                    if (err) {
+                        return res.sendStatus(400);
+                        
+                        res.status(200).send('' + rows[0].seq);
+                    };
+                });
+            });
+        } else {
+            console.log("sql_insert : " + sql_insert);
+            
+            db.get().query(sql_insert, [phone, name, sextype, birthday], function (err, result) {
+                if (err) {
+                    return res.sendStatus(400);
+                }
+                
+                res.sendStatus(200).send('' + result.insertId);
+            });
         }
-        
-        db.get().query(sql_insert, phone, function (err, result) {
-            if (err) {
-                return res.sendStatus(400);
-            }
-            res.status(200).send('' + result.insertId);
-        });
     });
 });
 
